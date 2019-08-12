d948248c803c5c900828cea8f5b55726a8673f0d
Santiago <santiago.hernandez@wolox.co>
2019-08-12 10:19:43 -0500

    The user model is modified

--- app/models/user.js
+++ app/models/user.js
@@ -10,13 +10,10 @@ module.exports = (sequelize, DataTypes) => {
         type: DataTypes.STRING,
         allowNull: false
       },
-      username: {
-        type: DataTypes.STRING,
-        allowNull: false
-      },
       email: {
         type: DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        unique: true
       },
       password: {
         type: DataTypes.STRING,
